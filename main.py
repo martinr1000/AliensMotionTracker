839ad4042f32af4fac706a2332054263b81774b0
martinr1000 <martinr1000@hotmail.com>
2020-08-13 18:56:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #Imports
-import pygame, random, math, time, pygame.mixer, os
+import pygame, random, math, pygame.mixer, os
 import time
 import sys
 
@@ -24,10 +24,6 @@ from calibrationGraphics import CalibrationGraphics
 os.environ["SDL_FBDEV"] = "/dev/fb1"
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 
-if gpioAvailable:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
 buttonTimerStart = 0
 buttonTimerCurrent = 0
 buttonHoldTime = 0
@@ -35,6 +31,23 @@ stateString="TRACK"
 changeState=True
 numberOfButtonPresses=0
 buttonHoldTime=0
+#(optional LED) led pin number
+ledPin=27
+#(optional LED) initial state (TRUE is on FALSE is off)
+ledState=False
+#(optional LED) time for LED when it is on in seconds
+ledOnTime=1
+#(optional) time for LED when it is off in seconds
+ledOffTime=1
+
+if gpioAvailable:
+    GPIO.setmode(GPIO.BCM)
+    #setup GPIO for tracker button press
+    GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    #setup a blinking LED for the tracker
+    GPIO.setup(ledPin, GPIO.OUT)
+    #set the initial state of the led
+    GPIO.output(ledPin, ledState)
 
 def my_callback(channel):
 
@@ -76,79 +89,114 @@ currentState=True
 startupTimerStart=time.time()
 startupTimerCurrent=time.time()
 qPress = False
-
 os.system("fbcp &")
 
 #show the startup gracphics
 sg.draw()
 
-while done==False:
+#initialise the clock instance to track led timings
+startTime = time.time()
+changeLedState = True
 
-    if numberOfButtonPresses>1:
-        addContact = True
-        numberOfButtonPresses=0
-    else:
-        addContact = False
+try:
+    while done==False:
 
-    #process pygame events
-    keys=pygame.key.get_pressed()        
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
-            pass
-        if event.type == pygame.KEYDOWN:
-            if event.key==K_q:
-                qPress = True
-                buttonTimerStart=time.time()
-                buttonTimerCurrent=time.time()
-                buttonHoldTime=0
-            if event.key==K_x:
-                done=True
-            if event.key==K_UP:
-                addContact = True
-        if event.type == pygame.KEYUP:
-            if event.key==K_q:
-                qPress=False
-
-    if qPress:
-        buttonHoldTime = time.time() - buttonTimerStart
-
-    if buttonHoldTime>4:
-        buttonHoldTime=0
-        stateString="CALIBRATE"
-        ca.initCalibration()
-        calibrationStep = 0
-
-    if stateString=="CALIBRATE":
-        xy = ca.calibrate(calibrationStep)
-
-        if calibrationStep==0:
-            cg.initBackground()
-            cg.update(xy, ca)
-        if calibrationStep==500:
-            stateString="TRACK"
-        else:
-            cg.update(xy, ca)
-            calibrationStep=calibrationStep+1
+        endTime = time.time()
+        elapsedTime = endTime - startTime
         
-    else:
+        if ledState:
+           if elapsedTime >= ledOnTime:
+               #turn it off
+               changeLedState = True
+        else:
+           if elapsedTime >= ledOffTime:
+               #turn it on
+               changeLedState = True
 
-        #process tracker graphics
-        args = tg.update(wave_size, addContact, ca)
-        if args!=999:
-            #process audio
-            ta.update(wave_size, args)
+        if changeLedState:
+          #change the led state
+          if ledState:
+            ledState = False
+            GPIO.output(ledPin, ledState)    
+            #set the change state flag to false as we are awaiting the next state change
+            changeLedState = False
+            #initialise the clock instance to track led timings
+            startTime = time.time()
+          else:
+            ledState = True
+            GPIO.output(ledPin, ledState)    
+            #set the change state flag to false as we are awaiting the next state change
+            changeLedState = False
+            #initialise the clock instance to track led timings
+            startTime = time.time()
 
-            #check the wave size, if it's 16 then reset otherwise increment
-            if wave_size==15:
-                wave_size=0
+        if numberOfButtonPresses>1:
+            addContact = True
+            numberOfButtonPresses=0
+        else:
+            addContact = False
+
+        #process pygame events
+        keys=pygame.key.get_pressed()        
+        for event in pygame.event.get(): # User did something
+            if event.type == pygame.QUIT: # If user clicked close
+                done=True # Flag that we are done so we exit this loop
+                pass
+            if event.type == pygame.KEYDOWN:
+                if event.key==K_q:
+                    qPress = True
+                    buttonTimerStart=time.time()
+                    buttonTimerCurrent=time.time()
+                    buttonHoldTime=0
+                if event.key==K_x:
+                    done=True
+                if event.key==K_UP:
+                    addContact = True
+            if event.type == pygame.KEYUP:
+                if event.key==K_q:
+                    qPress=False
+
+        if qPress:
+            buttonHoldTime = time.time() - buttonTimerStart
+
+        if buttonHoldTime>4:
+            buttonHoldTime=0
+            stateString="CALIBRATE"
+            ca.initCalibration()
+            calibrationStep = 0
+
+        if stateString=="CALIBRATE":
+            xy = ca.calibrate(calibrationStep)
+
+            if calibrationStep==0:
+                cg.initBackground()
+                cg.update(xy, ca)
+            if calibrationStep==500:
+                stateString="TRACK"
             else:
-                wave_size+=1
+                cg.update(xy, ca)
+                calibrationStep=calibrationStep+1
+            
+        else:
 
-    my_clock.tick(21)
+            #process tracker graphics
+            args = tg.update(wave_size, addContact, ca)
+            if args!=999:
+                #process audio
+                ta.update(wave_size, args)
+
+                #check the wave size, if it's 16 then reset otherwise increment
+                if wave_size==15:
+                    wave_size=0
+                else:
+                    wave_size+=1
+
+        my_clock.tick(21)
+finally:
+    print "clean exit"
 
 scope.pySurface.fill((0,0,0))
 scope.screen.blit(scope.pySurface,(0,0))
+GPIO.cleanup()
 os.system("killall fbcp")
 pygame.quit()
-
